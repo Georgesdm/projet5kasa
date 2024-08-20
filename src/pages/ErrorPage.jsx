17adef33b92d30d8da0c7fbb5f74eefe01503744
georges <118487336+Georgesdm@users.noreply.github.com>
2024-08-20 12:00:50 +0200

    feat: Création de la page d'erreur 404
    - Ajout du composant ErrorPage dans pages/ErrorPage.jsx pour gérer les routes inexistantes.
    - Mise en place du style pour la page d'erreur avec error.scss dans src/styles.
    - Ajout d'un lien de redirection vers la page d'accueil sur la page d'erreur.
    - Assure que toutes les routes inexistantes redirigent correctement vers la page d'erreur.
    - Conformité avec le design de la maquette.

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/error.scss';
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>ErrorPage</h1>
+    <div className="error-page">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/">Retourner sur la page d'accueil</Link>
     </div>
   );
 };
