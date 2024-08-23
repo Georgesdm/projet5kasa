07e666c465285ca3038786fb00fe87a6594df9a3
georges <118487336+Georgesdm@users.noreply.github.com>
2024-08-23 20:31:35 +0200

    style: Ajustements pour la responsivité mobile et la cohérence de l'interface utilisateur
    - FicheLogementPage :
      - Modification de la taille et de l'espacement des étoiles de notation pour la vue mobile afin de respecter les spécifications de la maquette.
      - Édition de host-name-mobile pour afficher correctement le prénom et le nom avec un espacement approprié.
    
    - Collapse :
      - Mise à jour du header du collapse avec une hauteur fixe pour les vues desktop et mobile pour une meilleure cohérence de l'interface utilisateur.
      - Ajustement de la taille des chevrons dans les collapses pour correspondre aux maquettes.
    
    - Card :
      - Ajout d'un curseur pointeur sur les cartes de la page d'accueil pour améliorer l'expérience utilisateur.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -14,11 +14,11 @@ const aboutData = [
   },
   {
     title: "Service",
-    content: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+    content: "La qualité du service est au cour de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance."
   },
   {
     title: "Sécurité",
-    content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés."
+    content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
   }
 ];
 
