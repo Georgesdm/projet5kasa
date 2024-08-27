76f6b2053b740dd1fb033d0c9dd808ef7b0135fb
georges <118487336+Georgesdm@users.noreply.github.com>
2024-08-27 16:48:31 +0200

    style : Ajustements UI pour correspondre à la maquette
    - Modification de la largeur maximale des collapses pour un alignement optimal avec la maquette.
    - Ajout d'un br-mobile sur la page d'erreur pour ajuster le texte en version mobile.
    - Modifications des marges sur plusieurs éléments pour correspondre précisément aux spécifications de la maquette.'

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -7,7 +7,7 @@ const ErrorPage = () => {
     <div className="fade-in">
       <div className="error-page">
         <h1>404</h1>
-        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <p>Oups! La page que<br className="mobile-br" /> vous demandez n'existe pas.</p>
         <Link to="/">Retourner sur la page d'accueil</Link>
       </div>
     </div>
